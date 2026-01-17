b0476eeb70d75a8282f04bb10bbc64a1ff42ee50
Pavel Gev <pavelgeveiler@gmail.com>
2026-01-17 12:30:12 +0100

    Apache indexes - on/off

--- docker/apache/entrypoint.sh
+++ docker/apache/entrypoint.sh
@@ -2,9 +2,23 @@
 set -eu
 
 : "${BASE_DOMAIN:?BASE_DOMAIN is required}"
+: "${APACHE_INDEXES:=on}"
 
 BASE_DOMAIN="$(printf '%s' "$BASE_DOMAIN" | tr -d '[:space:]' | sed 's/\.$//')"
 CONF="/usr/local/apache2/conf/extra/storage.conf"
+
+case "$APACHE_INDEXES" in
+  on|ON|true|1)
+    INDEX_OPT="Indexes"
+    ;;
+  off|OFF|false|0)
+    INDEX_OPT="-Indexes"
+    ;;
+  *)
+    echo "Invalid APACHE_INDEXES value: $APACHE_INDEXES (use on/off)"
+    exit 1
+    ;;
+esac
 
 cat > "$CONF" <<EOF
 <VirtualHost *:80>
@@ -15,7 +29,7 @@ cat > "$CONF" <<EOF
     VirtualDocumentRoot "/var/www/%1"
 
     <Directory "/var/www">
-        Options Indexes
+        Options $INDEX_OPT
         AllowOverride None
         Require all granted
         DirectoryIndex disabled
